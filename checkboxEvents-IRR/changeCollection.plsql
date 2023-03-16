4a517d71e7e4f508c87f90118eec9c5c013de784
GabrielZambonidaSilva <73555749+GabrielZambonidaSilva@users.noreply.github.com>
2023-03-15 21:37:47 -0300

    Add files via upload
    Signed-off-by: GabrielZambonidaSilva <73555749+GabrielZambonidaSilva@users.noreply.github.com>

---
+++ checkboxEvents-IRR/changeCollection.plsql
@@ -0,0 +1,179 @@
+-->> Nome do Callback deve ser CHANGE_COLLECION
+
+DECLARE
+    V_TYPE              APEX_APPLICATION.G_X01%TYPE := APEX_APPLICATION.G_X01;
+    V_SEQ_ID            INTEGER;
+    V_COLLECTION_NAME   VARCHAR2(250);
+    V_IDS               CLOB;
+    V_IDS_LIST          CLOB;
+    V_C001              VARCHAR2(1000);
+    V_ARR               APEX_APPLICATION.G_F01%TYPE := APEX_APPLICATION.G_F01 ;
+
+
+    V_JSON_ARR          JSON_ARRAY_T;
+    V_JSON_ARR_IDS      JSON_ARRAY_T;
+    V_JSON_OBJ          JSON_OBJECT_T;
+
+    V_JSON_RESPONSE     JSON_OBJECT_T := NEW JSON_OBJECT_T;
+    V_JSON_ARR_RESPONSE JSON_ARRAY_T := NEW JSON_ARRAY_T;
+
+
+BEGIN
+    -->> VERIFICO O TIPO DE SOLICITAÇÃO
+    IF UPPER(V_TYPE) = 'CREATE' THEN
+        V_JSON_ARR := JSON_ARRAY_T(
+            V_ARR(1)
+        );
+        
+        FOR I IN 0..V_JSON_ARR.GET_SIZE - 1 LOOP
+            V_JSON_OBJ := JSON_OBJECT_T(V_JSON_ARR.GET(I));
+            -->> VALIDO SE A COLLECTION EXISTE
+            IF NOT APEX_COLLECTION.COLLECTION_EXISTS(
+                P_COLLECTION_NAME => V_JSON_OBJ.GET_STRING('collectionName')
+            ) THEN
+                APEX_COLLECTION.CREATE_COLLECTION(
+                    P_COLLECTION_NAME => V_JSON_OBJ.GET_STRING('collectionName')
+                );
+
+            ELSIF UPPER(V_JSON_OBJ.GET_STRING('clearCollectionOnPageLoad')) = 'TRUE' THEN
+                APEX_COLLECTION.CREATE_OR_TRUNCATE_COLLECTION(
+                    P_COLLECTION_NAME => V_JSON_OBJ.GET_STRING('collectionName')
+                );
+            END IF;
+        END LOOP;
+
+        HTP.P(
+            JSON_OBJECT(
+                'sucesso'       VALUE 'Collection Inicializada com Sucesso'
+            )
+        );
+
+
+    ELSIF UPPER(V_TYPE) = 'CHANGE' THEN
+        V_JSON_ARR := JSON_ARRAY_T(
+            V_ARR(1)
+        );
+        
+        FOR I IN 0..V_JSON_ARR.GET_SIZE - 1 LOOP
+            V_JSON_OBJ := JSON_OBJECT_T(V_JSON_ARR.GET(I));
+
+            V_COLLECTION_NAME := UPPER(V_JSON_OBJ.GET_STRING('collectionName'));
+            V_C001 := V_JSON_OBJ.GET_STRING('id');
+
+            BEGIN
+                SELECT 
+                    SEQ_ID
+                INTO V_SEQ_ID
+                FROM APEX_COLLECTIONS
+                    WHERE UPPER(COLLECTION_NAME) = V_COLLECTION_NAME
+                        AND C001 = V_C001;
+
+                APEX_COLLECTION.DELETE_MEMBER(
+                    P_COLLECTION_NAME => V_COLLECTION_NAME,
+                    P_SEQ => V_SEQ_ID
+                );
+            EXCEPTION
+                WHEN NO_DATA_FOUND THEN
+                    NULL;
+                WHEN OTHERS THEN
+                     NULL;
+            END;
+
+            
+            
+            IF UPPER(V_JSON_OBJ.GET_STRING('checked')) = 'TRUE' THEN
+                APEX_COLLECTION.ADD_MEMBER(
+                    P_COLLECTION_NAME => V_COLLECTION_NAME,
+                    P_C001 => V_C001
+                );
+            END IF;
+        END LOOP;
+
+        HTP.P(
+            JSON_OBJECT(
+                'sucesso'    VALUE 'Collection Atualizada - ' || V_JSON_ARR.GET_SIZE || ' Itens'
+            )
+        );
+
+    -->> Devolvo um array com todos os que estão na página e contém na collection
+    ELSIF UPPER(V_TYPE) = 'LIST' THEN
+        V_JSON_ARR := JSON_ARRAY_T(
+            V_ARR(1)
+        );
+
+        FOR I IN 0..V_JSON_ARR.GET_SIZE - 1 LOOP
+            V_JSON_OBJ := JSON_OBJECT_T(V_JSON_ARR.GET(I));
+
+            V_COLLECTION_NAME := UPPER(V_JSON_OBJ.GET_STRING('collectionName'));
+            V_IDS := V_JSON_OBJ.GET_ARRAY('ids').TO_CLOB;
+            
+            BEGIN
+                SELECT
+                    JSON_ARRAYAGG(
+                        C001
+                    )
+                INTO V_IDS_LIST
+                FROM APEX_COLLECTIONS
+                    WHERE UPPER(COLLECTION_NAME) = V_COLLECTION_NAME
+                     AND C001 IN (
+                         SELECT COLUMN_VALUE FROM APEX_STRING.SPLIT(REPLACE(REPLACE(REPLACE(V_IDS,'[',''),']',''),'"',''), ',')
+                     );
+            EXCEPTION
+                WHEN NO_DATA_FOUND THEN
+                    NULL;
+            END;
+
+        
+            V_JSON_ARR_RESPONSE.APPEND(
+                JSON_OBJECT_T(
+                    JSON_OBJECT(
+                        'collectionName'        VALUE V_JSON_OBJ.GET_STRING('collectionName'),
+                        'ids'                   VALUE  V_IDS_LIST
+                    )
+                )
+            );
+
+        END LOOP;
+
+        htp.p(
+            V_JSON_ARR_RESPONSE.TO_CLOB
+        );
+    
+    -->> DELETE MEMBERS COLLECTION
+    ELSIF UPPER(V_TYPE) = 'DELETE' THEN
+        V_JSON_OBJ := JSON_OBJECT_T.PARSE(
+            V_ARR(1)
+        );
+
+        V_COLLECTION_NAME := V_JSON_OBJ.GET_STRING('collectionName');
+
+        FOR I IN (
+            SELECT SEQ_ID FROM APEX_COLLECTIONS 
+                WHERE UPPER(COLLECTION_NAME) = UPPER(V_COLLECTION_NAME)
+        ) LOOP
+            APEX_COLLECTION.DELETE_MEMBER(
+                P_COLLECTION_NAME => V_COLLECTION_NAME,
+                P_SEQ => I.SEQ_ID
+            );
+        END LOOP;
+
+        HTP.P(
+            JSON_OBJECT(
+                'collectionName'        VALUE V_COLLECTION_NAME,
+                'sucesso'               VALUE 'Deletado todos os membros'
+            )
+        );
+    END IF;
+
+
+EXCEPTION 
+    WHEN OTHERS THEN
+        ROLLBACK;
+        HTP.P(
+            JSON_OBJECT(
+                'erro'      VALUE 'Erro ao realizar operação',
+                'trace'     VALUE DBMS_UTILITY.FORMAT_ERROR_BACKTRACE,
+                'back'      VALUE SQLERRM
+            )
+        );
+END;
---
+++ checkboxEvents-IRR/checkIRR.js
@@ -0,0 +1,395 @@
+let elemCheckArr = [
+    {
+        "queryCheckAll": ".selectAllA",
+        "collectionName": "checkbox",
+        "buttonClear": "#button",
+        "clearCollectionOnPageLoad": false,
+        "checkColumn": {
+            "queryCheck": ".selectChild",
+            "attrValue": "data-id"
+        }
+    },
+    {
+        "queryCheckAll": ".selectAllB",
+        "collectionName": "checkboxB",
+        "buttonClear": "#button2",
+        "clearCollectionOnPageLoad": true,
+        "checkColumn": {
+            "queryCheck": ".selectChildb",
+            "attrValue": "data-id"
+        }
+    }
+]
+
+let staticRegion = '#staticRegion' + ' '
+
+/*
+    Funções que acionam o AJAX callback
+*/
+let elemObj = {
+    id: null,
+    checked: false,
+    collectionName: null
+}
+
+/**
+ * 
+ * @param {Array} elemCheckArr - Recebe o Objeto modelo [
+    {
+        "queryCheckAll": ".selectAllA",
+        "collectionName": "checkbox",
+        "buttonClear": "#button",
+        "clearCollectionOnPageLoad": false,
+        "checkColumn": {
+            "queryCheck": ".selectChild",
+            "attrValue": "data-id"
+        }
+    },
+    {
+        "queryCheckAll": ".selectAllB",
+        "collectionName": "checkboxB",
+        "buttonClear": "#button2",
+        "clearCollectionOnPageLoad": true,
+        "checkColumn": {
+            "queryCheck": ".selectChildb",
+            "attrValue": "data-id"
+        }
+    }
+] - Para que acione o callback de gerenciamento das collections (Criação / Deleteção)
+ */
+
+function createCollection(elemCheckArr){
+    let arrayCollection = []
+
+    elemCheckArr.forEach((e) => {   
+        arrayCollection.push({collectionName: e.collectionName, clearCollectionOnPageLoad: e.clearCollectionOnPageLoad})
+    })
+
+    apex.server.process(
+        'CHANGE_COLLECTION',
+        {
+            x01: 'CREATE',
+            f01: JSON.stringify(arrayCollection)
+        }
+    ).done((e) => {
+        console.log(e)
+    }).fail((e) => {
+        console.warn(e)
+    })
+}
+
+
+
+/**
+ * 
+ * @param {Array} elemCheck -  Array com o(s) Elementos para ser Alterados - Neste Array deve conter o modelo elemObj
+ */
+function changeCollection(elemCheck){
+    let elemArray = Array.from(elemCheck)
+    //Desabilito para evitar os cliques
+    elemArray.forEach((e) => {
+        eventCheckBox(
+            {
+                staticRegion: staticRegion, 
+                queryCheckAll: elemCheckArr.filter(elem => elem.collectionName == e.collectionName)[0].queryCheckAll, 
+                queryCheck: elemCheckArr.filter(elem => elem.collectionName == e.collectionName)[0].checkColumn.queryCheck, 
+                disable: true
+            }
+        )
+    })
+    
+
+    apex.server.process(
+        'CHANGE_COLLECTION',
+        {
+            x01: 'CHANGE',
+            f01: JSON.stringify(elemArray)
+        }
+    ).done((e) => {
+        console.log(e)
+    }).fail((e) => {
+        console.warn(e)
+    }).always((e) => {
+        //Habilito para evitar os cliques
+        elemArray.forEach((e) => {
+            eventCheckBox(
+                {
+                    staticRegion: staticRegion, 
+                    queryCheckAll: elemCheckArr.filter(elem => elem.collectionName == e.collectionName)[0].queryCheckAll, 
+                    queryCheck: elemCheckArr.filter(elem => elem.collectionName == e.collectionName)[0].checkColumn.queryCheck, 
+                    disable: false
+                }
+            )
+        })
+    })
+}
+
+
+/**
+ * @param {Array[]} elemCheckArr - Recebe um array de elementos no formato - 
+[
+    {
+        "queryCheckAll": ".selectAllA",
+        "collectionName": "checkbox",
+        "buttonClear": "#button",
+        "clearCollectionOnPageLoad": false,
+        "checkColumn": {
+            "queryCheck": ".selectChild",
+            "attrValue": "data-id"
+        }
+    },
+    {
+        "queryCheckAll": ".selectAllB",
+        "collectionName": "checkboxB",
+        "buttonClear": "#button2",
+        "clearCollectionOnPageLoad": true,
+        "checkColumn": {
+            "queryCheck": ".selectChildb",
+            "attrValue": "data-id"
+        }
+    }
+]
+ */
+function listCollection(elemCheckArr){
+
+    let elemArray = Array.from(elemCheckArr)
+    let listArray = []
+
+    elemArray.forEach((e) => {
+        console.log(e)
+        listArray.push(
+            {
+                collectionName: e.collectionName, 
+                ids: Array.from($(staticRegion + e.checkColumn.queryCheck)).map(
+                    (elem) => {
+                        return $(elem).attr(e.checkColumn.attrValue)
+                    } 
+                )
+            }
+        )
+    })
+    
+    apex.server.process(
+        'CHANGE_COLLECTION',
+        {
+            x01: 'LIST',
+            f01: JSON.stringify(listArray)
+        }
+    ).done((e) => {
+        //verifico se o retorno é um array de objetos
+        if (Array.isArray(e)){
+            
+            //Em cada objeto faço o parse do array encontrado e faço a marcação das checkbox na tela
+            Array.from(e).forEach(e => { 
+
+                let typeCheck = elemArray.filter(elemArr => elemArr.collectionName == e.collectionName)[0]
+
+                Array.from(JSON.parse(e.ids) ?? []).forEach((elem) => {
+                    
+                    $(
+                        `${staticRegion}${typeCheck?.checkColumn.queryCheck}[${typeCheck?.checkColumn.attrValue}="${elem}"]`    
+                    ).prop('checked', true)                        
+                })
+
+                //Desmarco todas as checkbox caso não tenha nenhuma na collection
+                if (Array.from(JSON.parse(e.ids) ?? []).length == 0){
+                    $(
+                        `${staticRegion}${typeCheck?.checkColumn.queryCheck}`
+                    ).prop('checked', false)
+                }
+            })
+        }
+
+    }).fail((e) => {
+        console.warn(e)
+    }).always((e) => {
+        verifyStateCheckAll(elemCheckArr)
+    })
+}
+
+
+/**
+ * @param {Object} obj - Recebe um obj de elementos no formato - {collectionName: "checkbox"}
+ */
+function deleteCollection(obj){
+    apex.server.process(
+        'CHANGE_COLLECTION',
+        {
+            x01: 'DELETE',
+            f01: JSON.stringify(obj)
+        }
+    ).done((e) => {
+        listCollection(elemCheckArr)
+        console.log(e)
+    }).fail((e) => {
+        listCollection(elemCheckArr)
+        console.warn(e)
+    })
+}
+
+
+
+/**
+ * 
+ * @param {Object} obj - Recebe o objeto modelo {
+        "queryCheckAll": ".selectAllB",
+        "collectionName": "checkboxB",
+        "buttonClear": "#button2",
+        "clearCollectionOnPageLoad": true,
+        "checkColumn": {
+            "queryCheck": ".selectChildb",
+            "attrValue": "data-id"
+        }
+    } - é usado para identificar qual a checkbox que está sendo modificada e atribuir o valor correto para o checkAll
+ */
+function stateCheckAll(obj){
+    let checkLine = $(staticRegion + obj.checkColumn.queryCheck)
+    let checkLineChecked = $(staticRegion + obj.checkColumn.queryCheck + ':checked')
+
+    checkLine.length == checkLineChecked.length
+        ? $(staticRegion + obj.queryCheckAll).prop('checked', true)
+        : $(staticRegion + obj.queryCheckAll).prop('checked', false)  
+}
+
+/**
+ * 
+ * @param {Array} elemCheckArr - Recebe o Objeto modelo [
+    {
+        "queryCheckAll": ".selectAllA",
+        "collectionName": "checkbox",
+        "buttonClear": "#button",
+        "clearCollectionOnPageLoad": false,
+        "checkColumn": {
+            "queryCheck": ".selectChild",
+            "attrValue": "data-id"
+        }
+    },
+    {
+        "queryCheckAll": ".selectAllB",
+        "collectionName": "checkboxB",
+        "buttonClear": "#button2",
+        "clearCollectionOnPageLoad": true,
+        "checkColumn": {
+            "queryCheck": ".selectChildb",
+            "attrValue": "data-id"
+        }
+    }
+] - Para que acione a função do checkAll e deixe o status dele de acordo com o disponivel em tela
+ */
+function verifyStateCheckAll(elemCheckArr) {
+    elemCheckArr.forEach((e) => {
+        stateCheckAll(e)
+    })
+}
+
+/**
+ * @param {Object} obj - Recebe o Objeto Modelo {staticRegion: "regiao", queryCheckAll: ".selectAll", queryCheck: "check", disable: true or false}
+ */
+function eventCheckBox(obj){
+    $(`${obj.staticRegion}${obj.queryCheckAll}`).attr('disabled', obj.disable ?? false)
+    $(`${obj.staticRegion}${obj.queryCheck}`).attr('disabled', obj.disable ?? false)
+}
+
+/**
+ * 
+ * @param {Array} elemCheckArr - Recebe o Objeto modelo [
+    {
+        "queryCheckAll": ".selectAllA",
+        "collectionName": "checkbox",
+        "buttonClear": "#button",
+        "clearCollectionOnPageLoad": false,
+        "checkColumn": {
+            "queryCheck": ".selectChild",
+            "attrValue": "data-id"
+        }
+    },
+    {
+        "queryCheckAll": ".selectAllB",
+        "collectionName": "checkboxB",
+        "buttonClear": "#button2",
+        "clearCollectionOnPageLoad": true,
+        "checkColumn": {
+            "queryCheck": ".selectChildb",
+            "attrValue": "data-id"
+        }
+    }
+] - Para que faça as buscas e atribua o evento correto a cada checkbox
+ */
+function setEventElements(elemCheckArr) {
+    let elemArrayState = []
+
+    elemCheckArr.forEach((e) => { 
+        //Add Event in CheckAll
+        if (e.queryCheckAll?.length > 0) {
+            $(staticRegion + e.queryCheckAll).on('click', (elem) => {
+                elem.stopPropagation()
+                $(staticRegion + e.checkColumn.queryCheck).prop('checked', $(elem.target).is(':checked'))  
+                
+                $.map(
+                    $(staticRegion + e.checkColumn.queryCheck),
+                    function(elem){
+                        elemObj.id = $(elem).attr(e.checkColumn.attrValue)
+                        elemObj.checked = $(elem).is(':checked')
+                        elemObj.collectionName = e.collectionName
+                        
+                        elemArrayState.push(
+                            elemObj
+                        )
+
+                        elemObj = {
+                            id: null,
+                            checked: false,
+                            collectionName: null
+                        }
+                    }
+                )
+
+                changeCollection(elemArrayState)
+                elemArrayState = []
+
+            }) 
+        }
+        //Add Event in CheckLine
+        if (e.checkColumn?.queryCheck.length > 0) {
+            $(staticRegion + e.checkColumn.queryCheck).on('click', (elem) => {
+                elem.stopPropagation()
+                stateCheckAll(e)
+                
+                elemObj = {
+                    id: null,
+                    checked: false,
+                    collectionName: null
+                }
+
+                elemObj.id = $(elem.target).attr(e.checkColumn.attrValue)
+                elemObj.checked = $(elem.target).is(':checked')
+                elemObj.collectionName = e.collectionName
+
+                changeCollection([elemObj])
+            }) 
+        }
+        // Add Event in Button Check
+        if (e.buttonClear?.length > 0){
+            $(staticRegion + e.buttonClear).on('click', (elem) => {
+                elem.stopImmediatePropagation()
+                deleteCollection({collectionName: e.collectionName})
+            })     
+        }           
+    })
+}
+
+
+function init() {
+    //Faço a criação das collections
+    createCollection(elemCheckArr)
+    //Aciono a atribuição de eventos para os elementos da página
+    setEventElements(elemCheckArr)
+    //Adiciono Eventos na Região estatica
+    $(staticRegion).on('apexafterrefresh', () => {
+        listCollection(elemCheckArr); 
+        setEventElements(elemCheckArr);
+        // setTimeout(() => {verifyStateCheckAll(elemCheckArr)}, 500);
+    });
+
+}
+
